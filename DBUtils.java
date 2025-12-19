e2c0cb8293eb78c8592f122269ac64b05987b843
Ranjitinfinity <ranjitsingh2489@gmail.com>
2025-12-19 05:50:21 +0000

    DBUtill commited

--- DBUtils.java
+++ DBUtils.java
@@ -1,3 +1,5 @@
 This file contains the DB Related Script
 Hello Add this line for updating
 This line is adde into master branch..
+Hello Add this line for updating.
+Adde this line in development branch
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
+
 This file contains deployment logic
 adding this line to master branch.
+
+This file contains one deployment logic.
+This line is added into development branch.
