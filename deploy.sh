38bc409aebb25df809789c18599eb12fe267ca34
Ranjitinfinity <ranjitsingh2489@gmail.com>
2025-12-19 06:00:21 +0000

    Merge conflict fixed

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 This file contains deployment logic
 adding this line to master branch.
-=======
 This file contains one deployment logic.
 This line is added into development branch.
->>>>>>> development
